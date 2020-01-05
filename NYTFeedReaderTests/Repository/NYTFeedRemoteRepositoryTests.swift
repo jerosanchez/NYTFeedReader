716de95de3150281f3c600e60d941c647fb748cc
Jero Sanchez <jero.sanchez@icloud.com>
2020-01-05 11:48:31 -0600

    Complete UTs for NYTFeed remote repository

--- NYTFeedReaderTests/Repository/NYTFeedRemoteRepositoryTests.swift
+++ NYTFeedReaderTests/Repository/NYTFeedRemoteRepositoryTests.swift
@@ -14,25 +14,106 @@ class NYTFeedRemoteRepositoryTests: XCTestCase {
     // MARK: - FetchPlayers
     
     func test_FetchArticles_Viewed_UsesProperUrlString() {
+        let randomTimeWindow = TimeWindow.allCases.randomElement()!
+        
+        let expectedUrlString = "https://api.nytimes.com/svc/mostpopular/v2/viewed/\(randomTimeWindow.rawValue).json?api-key=\(AppConfig.Secrets.NYTApiKey)"
+        assertUrlString(articleType: .viewed, timeWindow: randomTimeWindow, sharingSources: [], expectedUrlString: expectedUrlString)
+    }
+    
+    func test_FetchArticles_Emailed_UsesProperUrlString() {
+        let randomTimeWindow = TimeWindow.allCases.randomElement()!
+        
+        let expectedUrlString = "https://api.nytimes.com/svc/mostpopular/v2/emailed/\(randomTimeWindow.rawValue).json?api-key=\(AppConfig.Secrets.NYTApiKey)"
+        assertUrlString(articleType: .emailed, timeWindow: randomTimeWindow, sharingSources: [], expectedUrlString: expectedUrlString)
+    }
+    
+    func test_FetchArticles_SharedWithNoSharingSource_UsesProperUrlString() {
+        let randomTimeWindow = TimeWindow.allCases.randomElement()!
+        
+        let expectedUrlString = "https://api.nytimes.com/svc/mostpopular/v2/shared/\(randomTimeWindow.rawValue).json?api-key=\(AppConfig.Secrets.NYTApiKey)"
+        assertUrlString(articleType: .shared, timeWindow: randomTimeWindow, sharingSources: [], expectedUrlString: expectedUrlString)
+    }
+    
+    func test_FetchArticles_SharedWithOneSharingSource_UsesProperUrlString() {
+        let randomTimeWindow = TimeWindow.allCases.randomElement()!
+        let randomSharingSource = SharingSource.allCases.randomElement()!
+        
+        let expectedUrlString = "https://api.nytimes.com/svc/mostpopular/v2/shared/\(randomTimeWindow.rawValue)/\(randomSharingSource.rawValue).json?api-key=\(AppConfig.Secrets.NYTApiKey)"
+        assertUrlString(articleType: .shared, timeWindow: randomTimeWindow, sharingSources: [randomSharingSource], expectedUrlString: expectedUrlString)
+    }
+    
+    func test_FetchArticles_SharedWithTwoSharingSources_UsesProperUrlString() {
+        let randomTimeWindow = TimeWindow.allCases.randomElement()!
+        
+        let expectedUrlString = "https://api.nytimes.com/svc/mostpopular/v2/shared/\(randomTimeWindow.rawValue).json?api-key=\(AppConfig.Secrets.NYTApiKey)"
+        assertUrlString(articleType: .shared, timeWindow: randomTimeWindow, sharingSources: [.twitter, .facebook], expectedUrlString: expectedUrlString)
+    }
+    
+    func test_FetchArticles_ReturnsFeedArticles() {
+        let networkingServiceSpy = NetworkingServiceSpy(jsonString: jsonFeedArticles())
+        let sut = NYTFeedRemoteRepository(networkingService: networkingServiceSpy)
+        
+        let expectation = XCTestExpectation(description: "Finish API call")
+        sut.fetchArticles(articleType: .viewed, timeWindow: .oneDayAgo, sharingSources: []) { result in
+            switch result {
+            case .success(let feedArticles):
+                XCTAssertEqual(1, feedArticles.count)
+                XCTAssertEqual(1, feedArticles.first!.media?.count)
+            case .failure:
+                XCTFail()
+            }
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 5)
+    }
+}
+
+// MARK: - Helpers
+
+extension NYTFeedRemoteRepositoryTests {
+    
+    private func assertUrlString(articleType: ArticleType, timeWindow: TimeWindow, sharingSources: [SharingSource], expectedUrlString: String, file: StaticString = #file, line: UInt = #line) {
+        
         let networkingServiceSpy = NetworkingServiceSpy()
         let sut = NYTFeedRemoteRepository(networkingService: networkingServiceSpy)
         
         let expectation = XCTestExpectation(description: "Finish API call")
         
-        let randomTimeWindow = TimeWindow.allCases.randomElement()!
-        sut.fetchArticles(articleType: .viewed, timeWindow: randomTimeWindow, sharingSources: []) { _ in
-            let expectedApiUrlString = "https://api.nytimes.com/svc/mostpopular/v2/viewed/\(randomTimeWindow.rawValue).json?api-key=YOUR_API_KEY"
-            XCTAssertEqual(expectedApiUrlString, networkingServiceSpy.apiUrlString)
+        sut.fetchArticles(articleType: articleType, timeWindow: timeWindow, sharingSources: sharingSources) { _ in
+            XCTAssertEqual(expectedUrlString, networkingServiceSpy.apiUrlString, file: file, line: line)
             expectation.fulfill()
         }
         
         wait(for: [expectation], timeout: 5)
     }
     
-    // TODO: add more unit tests
+    private func jsonFeedArticles() -> String {
+        return """
+        {
+          "results": [
+            {
+              "url": "https://www.nytimes.com/2020/01/03/world/middleeast/iranian-general-qassem-soleimani-killed.html",
+              "section": "World",
+              "byline": "",
+              "type": "Article",
+              "title": "The Killing of Gen. Qassim Suleimani: What We Know Since the U.S. Airstrike",
+              "published_date": "2020-01-03",
+              "media": [
+                {
+                  "type": "image",
+                  "media-metadata": [
+                    {
+                      "url": "https://static01.nyt.com/images/2020/01/03/world/03iraq-briefing-span/merlin_166605342_bb1d07c1-25be-4a96-8815-857e98b24a47-square320.jpg"
+                    }                  ]
+                }
+              ]
+            }
+          ]
+        }
+        """
+    }
 }
-
-// MARK: - Helpers
 
 private class NetworkingServiceSpy: NetworkingService {
     
