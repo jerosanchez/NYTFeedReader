6c7915acf1ffd7eee24ac9b00455fa89418af84a
Jero Sanchez <jero.sanchez@icloud.com>
2020-01-05 08:02:59 -0600

    Initial Commit

---
+++ NYTFeedReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTFeedReader
+//
+//  Created by Jero Sánchez on 05/01/2020.
+//  Copyright © 2020 Jero Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
